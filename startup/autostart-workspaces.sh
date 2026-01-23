f98d948df4945e9c72d83890aba26197a87b2965
Nejc Carl <nejc.carl@gmail.com>
2026-01-23 04:44:17 +0100

    wip

--- startup/autostart-workspaces.sh
+++ startup/autostart-workspaces.sh
@@ -10,17 +10,19 @@ chromium &
 google-chrome &
 opera &
 spotify &
-webstorm &
+#webstorm &
+pycharm &
 clementine &
 
 # move apps
-sleep 5
+sleep 10
 wmctrl -x -r "brave-browser" -t 0
 wmctrl -x -r "spotify" -t 5
 wmctrl -x -r "spotify" -b add,fullscreen
 
 sleep 15
-wmctrl -x -r "webstorm" -t 10
+#wmctrl -x -r "webstorm" -t 10
+wmctrl -x -r "pycharm" -t 10
 wmctrl -x -r "google-chrome" -t 19
 wmctrl -x -r "chromium" -t 24
 wmctrl -x -r "opera" -t 25
